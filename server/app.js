15b2bc592cc8b6e613c8e8bd488d83a776862f52
Luis Barriga Carrasco <luisbarrigacarrasco@gmail.com>
2023-01-04 17:39:22 +0100

    Change package and app files to deploy

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import postsRoutes from "./routes/posts.routes.js";
 import fileUpload from "express-fileupload";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url)); // To deploy in Heroku
 
 // Middlewares
 app.use(express.json());
@@ -15,5 +18,8 @@ app.use(
 
 // Routes
 app.use(postsRoutes);
+
+// Static files
+app.use(express.static(join(__dirname, "../client/build")));
 
 export default app;
